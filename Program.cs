2372dbf4266d6b1ca6a4734fe49917e5eb98f593
Wolfman13 <34652519+Wolfman13@users.noreply.github.com>
2020-05-03 00:57:58 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    private static void WriteToFile(List<string> list, int currentFile)
+    {
+        string outputPath = Environment.CurrentDirectory.ToString() + $"\\Files\\File{currentFile}.txt";
+        File.AppendAllLines(outputPath, list);
+    }
+
+    public static void Main()
+    {
+        int currentFile = 1;
+        string inputFile = Environment.CurrentDirectory.ToString() + "\\Words.txt";
+
+        if (File.Exists(inputFile))
+        {
+            List<string> list = new List<string>();
+
+            Console.WriteLine("Reading to list.txt");
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    list.Add(reader.ReadLine());
+
+                    if (list.Count >= 10000000)
+                    {
+                        Console.WriteLine("Writing to file.");
+                        WriteToFile(list, currentFile);
+
+                        list = new List<string>();
+                        currentFile++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Writing to file.");
+            WriteToFile(list, currentFile);
+
+            Console.WriteLine($"Finished splitting file.");
+        }
+
+        Console.WriteLine("Press any key to continue...");
+        _ = Console.ReadKey();
+    }
+}
